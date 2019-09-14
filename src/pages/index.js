2a789cba9c7e977963a8fa387fc54fb5dd4fae99
Your Name Here <jennifernhf@gmail.com>
2019-09-14 15:54:38 -0500

    rm image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <p>home page</p>
-    <Image /> 
   </Layout>
 )
 
